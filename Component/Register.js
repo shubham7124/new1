4f6a814f49faa17e3201333260ea66ebf70640e2
shubham7124 <shubhamsutar1182@gmail.com>
2025-08-19 18:46:30 +0530

    validation added

--- Component/Register.js
+++ Component/Register.js
@@ -8,132 +8,79 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [confiPassword, setConfiremedPassword] = useState("");
 
-  const Show=(e)=>{
-      
-    console.log(name)
-    console.log(email)
-    console.log(password)
-    console.log(confiPassword)
-  }
+  const [errors, setErrors] = useState({});
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let tempErrors = {};
+
+    // Name validation
+    if (name === "") tempErrors.name = "Name is required";
+    else if (name.length < 2) tempErrors.name = "Name must be at least 2 characters";
+
+   
+
+    setErrors(tempErrors);
+
+    // If no errors, form is valid
+    if (Object.keys(tempErrors).length === 0) {
+      alert("Registration Successful!");
+      console.log("Name:", name);
+     
+
+      // Reset form
+      setName(""); 
+      setErrors({});
+    }
+  };
 
   return (
     <div>
       <h3>Register</h3>
-      <label>
-        User Name:
-        <input
-          placeholder="Your Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <br></br>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label><br />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {errors.name && <div style={{ color: "red" }}>{errors.name}</div>}
+        </div>
 
-     <label>
-      Email:
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      </label> <br></br>
+        <div>
+          <label>Email:</label><br />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors.email && <div style={{ color: "red" }}>{errors.email}</div>}
+        </div>
 
-      <label>
-      Password:
-      <input
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      </label><br></br>
+        <div>
+          <label>Password:</label><br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && <div style={{ color: "red" }}>{errors.password}</div>}
+        </div>
 
-      <label>
-      confiPassword:
-      <input
-        placeholder="Confirm Password"
-        type="password"
-        value={confiPassword}
-        onChange={(e) => setConfiremedPassword(e.target.value)}
-      />
-       </label><br></br>
+        <div>
+          <label>Confirm Password:</label><br />
+          <input
+            type="password"
+            value={confiPassword}
+            onChange={(e) => setConfiremedPassword(e.target.value)}
+          />
+          {errors.confiPassword && <div style={{ color: "red" }}>{errors.confiPassword}</div>}
+        </div>
 
-       <button onClick={Show}>submit</button>
-      
+        <button type="submit">Submit</button>
+      </form>
     </div>
-
-    // updated  design for from  of registratin 
-    //     <div>
-    //   <h3>Register</h3>
-    //   <form onSubmit={handleSubmit}>
-    //     <table>
-    //       <tbody>
-    //         <tr>
-    //           <td>
-    //             <label>User Name:</label>
-    //           </td>
-    //           <td>
-    //             <input
-    //               placeholder="Your Name"
-    //               value={name}
-    //               onChange={(e) => setName(e.target.value)}
-    //             />
-    //           </td>
-    //         </tr>
-
-    //         <tr>
-    //           <td>
-    //             <label>Email:</label>
-    //           </td>
-    //           <td>
-    //             <input
-    //               placeholder="Email"
-    //               value={email}
-    //               onChange={(e) => setEmail(e.target.value)}
-    //             />
-    //           </td>
-    //         </tr>
-
-    //         <tr>
-    //           <td>
-    //             <label>Password:</label>
-    //           </td>
-    //           <td>
-    //             <input
-    //               placeholder="Password"
-    //               type="password"
-    //               value={password}
-    //               onChange={(e) => setPassword(e.target.value)}
-    //             />
-    //           </td>
-    //         </tr>
-
-    //         <tr>
-    //           <td>
-    //             <label>Confirm Password:</label>
-    //           </td>
-    //           <td>
-    //             <input
-    //               placeholder="Confirm Password"
-    //               type="password"
-    //               value={confiPassword}
-    //               onChange={(e) => setConfiremedPassword(e.target.value)}
-    //             />
-    //           </td>
-    //         </tr>
-
-    //         <tr>
-    //           <td colSpan="2" style={{ textAlign: "center", paddingTop: "10px" }}>
-    //             <button type="submit">Submit</button>
-    //           </td>
-    //         </tr>
-    //       </tbody>
-    //     </table>
-    //   </form>
-
-    //   {/* Preview of Name */}
-    //   <h4>{name}</h4>
-    // </div>
   );
 }
